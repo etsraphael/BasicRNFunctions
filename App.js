61d2a58853f440fca129df11aa8f9eba977129d7
etsraphael <ets_raphael@outlook.fr>
2022-11-29 20:28:04 +1000

    flow config added

--- App.js
+++ App.js
@@ -1,36 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
 import type { Node } from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  SafeAreaView, StyleSheet,
   Text,
   useColorScheme,
-  View,
-  Image,
-  Button
+  View
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 import MainApp from './app/page/main-app.js';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
 const Section = ({ children, title }): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -66,37 +46,39 @@ const App: () => Node = () => {
 
   // send file to server
   const sendFile = async () => {
-
-    console.log('starting')
+    console.log('starting');
 
     // create blob from image
 
-    const uri = 'https://images.unsplash.com/photo-1669557812723-665dff7e92b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80';
+    const uri =
+      'https://images.unsplash.com/photo-1669557812723-665dff7e92b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80';
     const response = await fetch(uri);
     const imgBlob = await response.blob();
 
-
-    fetch("https://pod-000-1400-06.backblaze.com/b2api/v2/b2_upload_file/ea230fff2fc2af978a400f12/c000_v0001400_t0046", {
-      body: imgBlob,
-      headers: {
-        Authorization: "4_000a3fff2f7a0f20000000005_01a8a189_cd0170_upld_f3Q0ZNHfYw6QuoMXFMWM-q5c2q4=",
-        "Content-Type": "image/jpeg",
-        "X-Bz-Content-Sha1": "do_not_verify",
-        "X-Bz-File-Name": "first-file21",
-        "X-Bz-Info-Author": "unknown",
+    fetch(
+      'https://pod-000-1400-06.backblaze.com/b2api/v2/b2_upload_file/ea230fff2fc2af978a400f12/c000_v0001400_t0046',
+      {
+        body: imgBlob,
+        headers: {
+          Authorization:
+            '4_000a3fff2f7a0f20000000005_01a8a189_cd0170_upld_f3Q0ZNHfYw6QuoMXFMWM-q5c2q4=',
+          'Content-Type': 'image/jpeg',
+          'X-Bz-Content-Sha1': 'do_not_verify',
+          'X-Bz-File-Name': 'first-file21',
+          'X-Bz-Info-Author': 'unknown',
+        },
+        method: 'POST',
       },
-      method: "POST"
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log('response2', response)
-      }).catch((error) => {
-        //check the response
-        console.log('error', error)
+    )
+      .then(response => response.json())
+      .then(response => {
+        console.log('response2', response);
       })
-
-
-  }
+      .catch(error => {
+        //check the response
+        console.log('error', error);
+      });
+  };
 
   return (
     // <SafeAreaView style={backgroundStyle}>
@@ -117,7 +99,6 @@ const App: () => Node = () => {
     //         screen and then come back to see your edits.
     //       </Section>
 
-
     //       <Image
     //         style={styles.logo}
     //         source={{
@@ -127,13 +108,11 @@ const App: () => Node = () => {
 
     //       <Button title="Send File" onPress={sendFile} />
 
-
-
     //     </View>
     //   </ScrollView>
     //   <MainApp/>
     // </SafeAreaView>
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <MainApp />
     </SafeAreaView>
   );
